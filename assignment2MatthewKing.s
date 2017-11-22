b23e211a61592d0bb8e6a49ef5dae21e0eef3c60
MatthewKing98 <matthewsky98@gmail.com>
2017-11-21 23:18:40 -0500

    Update assignment2MatthewKing.s
    defined spaces for user input and for unsigned integer output

--- assignment2MatthewKing.s
+++ assignment2MatthewKing.s
@@ -6,3 +6,10 @@
 ## FUNCTION: Converts strings of Hex to strings of Dec    ##
 ############################################################
 ############################################################
+
+.data
+	userInput: #location were valid entry is stored
+		.space 1001 #9 bytes, 1 byte per possible character, and an end-of-string marker; Space is maximum size
+		
+	outputDecimal: #string version of number (stored here because the 32 bit register represents 2s compliment vs unsigned)
+		.space 10 #Largest expected value is 4294967295 (FFFFFFFF); 10 digits long
